048ba466550adf961037dbbe990a3e1b47d68ef7
Nero <karta0706566@gmail.com>
2023-01-24 09:07:50 +0800

    feat: add & set the toenv

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 
+const mongoose = require('mongoose') // 引入mongoose
+if (process.env.NODE_ENV !== 'production'){ // 若node環境變數非production 
+  require('dotenv').config() // 引用dotenv使用.config()函數
+}
+mongoose.connect(process.env.MONGODB_URI) // 以mongoose建立連線
+
+const db = mongoose.connection // 將連線狀況宣告為db
+db.on('error',()=>{ // 若error產生
+  console.log('mongodb error!')
+})
+db.once('open',()=>{ // 若成功連線
+  console.log('mongodb connected!')
+})
+
 // 設定固定樣板
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
