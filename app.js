00ce0af4f9f912ef25fecf35aba10bbad2a33e07
Nero <karta0706566@gmail.com>
2023-01-24 09:40:45 +0800

    feat: add the seeder

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 
 const mongoose = require('mongoose') // 引入mongoose
-if (process.env.NODE_ENV !== 'production'){ // 若node環境變數非production 
+if (process.env.NODE_ENV !== 'production') { // 若node環境變數非production 
   require('dotenv').config() // 引用dotenv使用.config()函數
 }
-mongoose.connect(process.env.MONGODB_URI) // 以mongoose建立連線
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 以mongoose建立連線
 
 const db = mongoose.connection // 將連線狀況宣告為db
-db.on('error',()=>{ // 若error產生
+db.on('error', () => { // 若error產生
   console.log('mongodb error!')
 })
-db.once('open',()=>{ // 若成功連線
+db.once('open', () => { // 若成功連線
   console.log('mongodb connected!')
 })
 
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurants')
+const restaurantList = require('../../restaurant.json').results
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error',()=>{
+  console.log('mongodb error!')
+})
+db.once('open',()=>{
+  console.log('mongodb connected!')
+
+  Restaurant.create(restaurantList)
+})
